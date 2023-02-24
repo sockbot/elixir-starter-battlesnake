f6267608a74b1e019c6dae66f70e2de844e61f19
Andy Chan <1291473+sockbot@users.noreply.github.com>
2023-02-24 14:38:11 -0800

    write GET / test

--- test/elixir_starter_battlesnake_test.exs
+++ test/elixir_starter_battlesnake_test.exs
@@ -1,8 +1,18 @@
-defmodule ElixirStarterBattlesnakeTest do
+defmodule RouterTest do
   use ExUnit.Case
-  doctest ElixirStarterBattlesnake
+  use Plug.Test
 
-  test "greets the world" do
-    assert ElixirStarterBattlesnake.hello() == :world
+  alias ElixirStarterBattlesnake.Router
+
+  doctest Router
+
+  test "GET /" do
+    conn = conn(:get, "/")
+    conn = Router.call(conn, [])
+
+    assert conn.status == 200
+
+    assert conn.resp_body ==
+             "{\"version\":\"0.0.1-beta\",\"tail\":\"default\",\"head\":\"default\",\"color\":\"#888888\",\"author\":\"MyUsername\",\"apiversion\":\"1\"}"
   end
 end
