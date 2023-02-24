7d8b7b5980e4d659ead69c38bb68ea193bb5335d
Andy Chan <1291473+sockbot@users.noreply.github.com>
2023-02-24 11:40:47 -0800

    create hello world plug

--- lib/elixir_starter_battlesnake/application.ex
+++ lib/elixir_starter_battlesnake/application.ex
@@ -4,17 +4,23 @@ defmodule ElixirStarterBattlesnake.Application do
   @moduledoc false
 
   use Application
+  require Logger
 
   @impl true
   def start(_type, _args) do
     children = [
       # Starts a worker by calling: ElixirStarterBattlesnake.Worker.start_link(arg)
       # {ElixirStarterBattlesnake.Worker, arg}
+      {Plug.Cowboy,
+       scheme: :http, plug: ElixirStarterBattlesnake.HelloWorldPlug, options: [port: 8080]}
     ]
 
     # See https://hexdocs.pm/elixir/Supervisor.html
     # for other strategies and supported options
     opts = [strategy: :one_for_one, name: ElixirStarterBattlesnake.Supervisor]
+
+    Logger.info("Starting snake...")
+
     Supervisor.start_link(children, opts)
   end
 end
